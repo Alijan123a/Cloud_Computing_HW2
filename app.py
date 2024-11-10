9dfe03726b0371fb67161369d9c1b32bee97fced
Alijan123a <91368365+Alijan123a@users.noreply.github.com>
2024-11-10 07:27:20 +0330

    Update app.py
    updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import redis
 
 app = Flask(__name__)
-cache = redis.StrictRedis(host='redis', port=6379, db=0)
+cache = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 API_KEY = "7yrnhzcTJk3vFd07nzSROA==vRxkEynraZ3Flbyn"
 BASE_URL = "https://api.api-ninjas.com/v1"
@@ -15,19 +15,22 @@ def define_word(*args):
         word = args[0]
     else:
         word = request.form["word"]
+    
     if not word:
         return jsonify({"error": "No word provided"}), 400
 
+    print(word)
     cached_definition = cache.get(word)
     if cached_definition:
-        return jsonify({"word": word, "definition": cached_definition.decode("utf-8")})
+        print(cached_definition)
+        return jsonify({"word": word, "definition(cached)": cached_definition})
 
     headers = {'X-Api-Key': API_KEY}
     response = requests.get(f"{BASE_URL}/dictionary?word={word}", headers=headers)
     if response.status_code == 200:
         definition = response.json().get("definition", "No definition found")
         cache.setex(word, 300, definition)
-        return jsonify({"word": word, "definition": definition})
+        return jsonify({"word": word, "definition(url)": definition})
     else:
         return jsonify({"error": "Failed to retrieve definition"}), 500
 
@@ -37,6 +40,7 @@ def random_word():
     headers = {'X-Api-Key': API_KEY}
     response = requests.get(f"{BASE_URL}/randomword", headers=headers)
     if response.status_code == 200:
+        print(response.json())
         word = response.json().get("word")
         return define_word(word[0])
     else:
