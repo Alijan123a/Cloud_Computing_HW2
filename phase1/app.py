d156ba53965076eacf1987271d394da1631a385a
Alijan123a <alijan2021m@gmail.com>
2024-11-26 01:14:45 +0330

    app file updated

--- phase1/app.py
+++ phase1/app.py
@@ -1,42 +1,93 @@
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
 import requests
 import redis
+import os
+from datetime import datetime
+import json
+
+
+load_dotenv()
+
+class Config:
+    API_KEY = os.getenv('API_NINJA_KEY')
+    BASE_URL = 'https://api.api-ninjas.com/v1'
+    REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
+    REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+    REDIS_CACHE_DURATION = int(os.getenv('REDIS_CACHE_DURATION', 300))
+    FLASK_PORT = int(os.getenv('FLASK_PORT', 5000))
+
 
 app = Flask(__name__)
-cache = redis.StrictRedis(host='redis', port=6379, db=0)
+config = Config()
+print(config.API_KEY)
+print(config.REDIS_CACHE_DURATION)
+cache = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, decode_responses=True)
 
-API_KEY = "7yrnhzcTJk3vFd07nzSROA==vRxkEynraZ3Flbyn"
-BASE_URL = "https://api.api-ninjas.com/v1"
+# API_KEY = "7yrnhzcTJk3vFd07nzSROA==vRxkEynraZ3Flbyn"
+# BASE_URL = "https://api.api-ninjas.com/v1"
 
-@app.route('/define', methods=['GET'])
-def define_word():
-    word = request.args.get('word')
+
+def save_to_cach(word, definition):
+    data = {
+        "definition" : definition,
+        "time" :  datetime.now().isoformat()
+    }
+    cache.setex(word, config.REDIS_CACHE_DURATION, json.dumps(data))
+
+
+@app.route('/define', methods=['POST'])
+def define_word(*args):
+    # print("define")
+    word = None
+    if args:
+        word = args[0]
+    else:
+        word = request.form["word"]
     if not word:
         return jsonify({"error": "No word provided"}), 400
 
-    cached_definition = cache.get(word)
-    if cached_definition:
-        return jsonify({"word": word, "definition": cached_definition.decode("utf-8")})
+    # GET DATA FROM CACHE
+    cached = cache.get(word)
+    print(cached)
+    if cached:
+        data = json.loads(cached)
+        return jsonify({
+            "word": word,
+            "definition": data['definition'], 
+            "isCached": "TRUE",
+            "time": data['time']
+        })
 
-    headers = {'X-Api-Key': API_KEY}
-    response = requests.get(f"{BASE_URL}/dictionary?word={word}", headers=headers)
+
+    # GET DATA FROM URL
+    headers = {'X-Api-Key': config.API_KEY}
+    response = requests.get(f"{config.BASE_URL}/dictionary?word={word}", headers=headers)
+
     if response.status_code == 200:
         definition = response.json().get("definition", "No definition found")
-        cache.setex(word, 300, definition)
-        return jsonify({"word": word, "definition": definition})
+        save_to_cach(word, definition)
+        return jsonify({
+            "word": word,
+            "definition": definition,
+            "isCached":"FALSE"
+        })
+    
     else:
         return jsonify({"error": "Failed to retrieve definition"}), 500
 
 
 @app.route('/randomword', methods=['GET'])
 def random_word():
-    headers = {'X-Api-Key': API_KEY}
-    response = requests.get(f"{BASE_URL}/randomword", headers=headers)
+    print("random")
+    headers = {'X-Api-Key': config.API_KEY}
+    print(config.API_KEY)
+    response = requests.get(f"{config.BASE_URL}/randomword", headers=headers)
     if response.status_code == 200:
         word = response.json().get("word")
-        return define_word()
+        return define_word(word[0])
     else:
         return jsonify({"error": "Failed to retrieve random word"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=config.FLASK_PORT)
